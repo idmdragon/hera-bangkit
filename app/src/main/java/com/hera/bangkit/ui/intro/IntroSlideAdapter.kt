176144788d576ebcf0d786ba5ee828f0f636ec20
Annisah <annisahamalia@student.ub.ac.id>
2021-05-21 21:52:16 +0700

    complete layouting intro

--- app/src/main/java/com/hera/bangkit/ui/intro/IntroSlideAdapter.kt
+++ app/src/main/java/com/hera/bangkit/ui/intro/IntroSlideAdapter.kt
@@ -7,37 +7,31 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.hera.bangkit.R
+import com.hera.bangkit.databinding.ActivityIntroBinding
 
 class IntroSlideAdapter (private val introSlide: List<IntroSlide>): RecyclerView.Adapter<IntroSlideAdapter.IntroSlideViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSlideViewHolder {
-        return IntroSlideViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.slide_item_container,parent,false
-            )
-
-        )
-    }
-
-    override fun getItemCount(): Int {
-        return introSlide.size
+        return IntroSlideViewHolder(LayoutInflater.from(parent.context).inflate(
+                R.layout.slide_item_container,parent,false))
     }
 
     override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
-        holder.bind(introSlide[position] )
+        val slide = introSlide[position]
+        holder.bind(slide)
     }
 
+    override fun getItemCount(): Int = introSlide.size
+
     inner class IntroSlideViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        private  val textTitle = view.findViewById<TextView>(R.id.textTitle)
-        private  val textDescription = view.findViewById<TextView>(R.id.textDescription)
-        private  val imageIcon = view.findViewById<ImageView>(R.id.imageSlideIcon)
+        private  val textTitle = view.findViewById<TextView>(R.id.tv_ilu_title)
+        private  val textDescription = view.findViewById<TextView>(R.id.tv_ilu_desc)
+        private  val imageIcon = view.findViewById<ImageView>(R.id.iv_ilu_intro)
 
         fun bind(introSlide: IntroSlide){
             textTitle.text = introSlide.title
             textDescription.text = introSlide.description
-            imageIcon.setImageResource(introSlide.icon )
-
+            imageIcon.setImageResource(introSlide.ilustration )
         }
     }
-
 }
