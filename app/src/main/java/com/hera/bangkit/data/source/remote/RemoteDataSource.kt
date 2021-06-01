ac44afeac60fda823c68760ef6cb1ee67d5338d5
Annisah <annisahamalia@student.ub.ac.id>
2021-06-01 14:26:00 +0700

    add NetworkBoundSource and update RemoteDataSource insertUser

--- app/src/main/java/com/hera/bangkit/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/hera/bangkit/data/source/remote/RemoteDataSource.kt
@@ -3,8 +3,13 @@ package com.idm.moviedb.data.source.remote
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.ktx.toObject
 import com.hera.bangkit.data.entity.StoryEntity
+import com.hera.bangkit.data.entity.UserEntity
+import com.hera.bangkit.data.response.ReportEntity
+import com.hera.bangkit.data.response.StoryResponse
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,17 +21,47 @@ import javax.inject.Named
 
 
 class RemoteDataSource @Inject constructor(
-    @Named("story")
-    private val storyCollection: DocumentReference,
+    @Named("stories")
+    private val storyCollection: CollectionReference,
     @Named("report")
-    private val reportCollection: DocumentReference,
+    private val reportCollection: CollectionReference,
+    @Named("users")
+    private val userCollection: CollectionReference
 
-    ) {
+) {
 
-    fun insertStory(storyEntity: StoryEntity) {
+    fun insertStory(storyEntity: StoryResponse) {
         CoroutineScope(Dispatchers.IO).launch {
-            storyCollection.set(storyEntity).await()
+            storyCollection.add(storyEntity).await()
         }
     }
 
+    fun insertReport(reportEntity: ReportEntity) {
+        CoroutineScope(Dispatchers.IO).launch {
+            reportCollection.add(reportEntity).await()
+        }
+    }
+
+    fun insertUser(user: UserEntity) {
+        CoroutineScope(Dispatchers.IO).launch {
+            userCollection.add(user).await()
+        }
+    }
+
+    fun getUserReport():LiveData<RemoteResponse<ReportEntity>>{
+        val listItems = MutableLiveData<RemoteResponse<ReportEntity>>()
+        CoroutineScope(Dispatchers.IO).launch {
+            val querySnapshot = reportCollection
+                .whereEqualTo("fullname","ilham")
+                .get()
+                .await()
+            for(document in querySnapshot.documents){
+                val item = document.toObject<ReportEntity>()
+                if (item!=null){
+                    listItems.postValue(RemoteResponse.success(item))
+                }
+            }
+        }
+        return listItems
+    }
 }
