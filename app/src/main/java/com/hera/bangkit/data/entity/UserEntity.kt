cd36e0fd9e5b0fab176d4343661630c80d6205b9
Annisah <annisahamalia@student.ub.ac.id>
2021-06-01 14:21:10 +0700

    update UserEntity

--- app/src/main/java/com/hera/bangkit/data/entity/UserEntity.kt
+++ app/src/main/java/com/hera/bangkit/data/entity/UserEntity.kt
@@ -1,11 +1,19 @@
 package com.hera.bangkit.data.entity
 
 data class UserEntity (
-        val Uid : String,
-        val NIK : String,
-        val Fullname : String,
-        val Username : String,
-        val PhoneNumber : String,
-        val DateOfBirth : String,
-        val Address : String,
-        )
+        val address : String,
+        val avatar : String,
+        val dateOfBirth: String,
+        val email: String,
+        val fullName: String,
+        val guardianName1: String,
+        val guardianName2: String,
+        val guardianPhoneNumber1: String,
+        val guardianPhoneNumber2: String,
+        val nik : String,
+        val password: String,
+        val phoneNumber: String,
+        val placeOfBirth: String,
+        val uid: String,
+        val username: String
+)
