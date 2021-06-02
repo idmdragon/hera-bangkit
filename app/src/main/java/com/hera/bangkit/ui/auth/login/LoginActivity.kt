7a60c73c53a6132b92ab3632ec56003c888061c9
Annisah <annisahamalia@student.ub.ac.id>
2021-06-02 15:41:14 +0700

    update logic on sos activity

--- app/src/main/java/com/hera/bangkit/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/hera/bangkit/ui/auth/login/LoginActivity.kt
@@ -1,36 +1,16 @@
 package com.hera.bangkit.ui.auth.login
 
-import android.content.ContentValues.TAG
-import android.content.DialogInterface
 import android.content.Intent
-import android.os.Build.ID
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.util.Patterns.EMAIL_ADDRESS
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
-import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.DialogFragmentNavigatorDestinationBuilder
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-//import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.auth.FirebaseUser
-import com.hera.bangkit.R
 import com.hera.bangkit.databinding.ActivityLoginBinding
 import com.hera.bangkit.ui.auth.register.RegisterActivity
-import com.hera.bangkit.ui.auth.register.registerfragment.Register1FragmentDirections
 import com.hera.bangkit.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
@@ -66,6 +46,7 @@ class LoginActivity : AppCompatActivity() {
                         if (it.isSuccessful) {
                             Toast.makeText(this@LoginActivity, "User Berhasil Masuk", Toast.LENGTH_SHORT).show()
                             checkUserId(it.getResult()!!.user!!.uid)
+
                         } else {
                             Toast.makeText(this@LoginActivity, "Email/Password Salah", Toast.LENGTH_SHORT).show()
                         }
@@ -87,7 +68,6 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, "Akun tidak ada", Toast.LENGTH_SHORT).show()
         }
     }
-
 
     fun register(view: View) {
         val intent = Intent(applicationContext, RegisterActivity::class.java)
